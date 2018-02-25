a5e8ab0f9bbb63e0782f43a67d8e374dba920bd7
webber <webber.nl@gmail.com>
2018-02-26 00:27:26 +0100

    Configure encore properly. Bump @symfony/webpack-encore version to fix IDE bug.

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,29 @@
-var Encore = require('@symfony/webpack-encore');
+const Encore = require('@symfony/webpack-encore');
 
 Encore
     // the project directory where compiled assets will be stored
     .setOutputPath('public/build/')
+
     // the public path used by the web server to access the previous directory
     .setPublicPath('/build')
+
+    // empty the output directory between each build
     .cleanupOutputBeforeBuild()
+
+    // render all final CSS and JS files with source maps to help debugging
     .enableSourceMaps(!Encore.isProduction())
-    // uncomment to create hashed filenames (e.g. app.abc123.css)
-    // .enableVersioning(Encore.isProduction())
 
-    // uncomment to define the assets of the project
-    // .addEntry('js/app', './assets/js/app.js')
-    // .addStyleEntry('css/app', './assets/css/app.scss')
+    // create hashed file names (e.g. app.abc123.css)
+    .enableVersioning(Encore.isProduction())
 
-    // uncomment if you use Sass/SCSS files
-    // .enableSassLoader()
+    // define the script assets
+    .addEntry('js/app', './assets/js/app.js')
 
-    // uncomment for legacy applications that require $/jQuery as a global variable
-    // .autoProvidejQuery()
+    // define style assets
+    .addStyleEntry('css/app', './assets/css/app.scss')
+
+    // use Sass/SCSS files
+    .enableSassLoader()
 ;
 
 module.exports = Encore.getWebpackConfig();
