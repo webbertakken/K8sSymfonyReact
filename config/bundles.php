efd02f5f00a50f551c922aa612217cceb007734c
webber <d.webbink@dation.nl>
2018-03-30 16:27:04 +0200

    Add fixtures and functional tests configuration

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,6 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Liip\FunctionalTestBundle\LiipFunctionalTestBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
