3083a67f0e1556ec196ed50bab73c24c70351a19
webber <d.webbink@dation.nl>
2018-07-19 18:28:03 +0200

    Add material-icons

--- assets/scenes/Demo/Materialize/index.jsx
+++ assets/scenes/Demo/Materialize/index.jsx
@@ -1,18 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'materialize-css/dist/js/materialize.min';
-import 'materialize-css/dist/css/materialize.min.css';
-import { Dropdown, Button } from 'react-materialize';
+import 'materialize-css';
+import {
+  Breadcrumb,
+  Dropdown,
+  Button,
+  Navbar,
+  NavItem,
+  Icon,
+  MenuItem,
+} from 'react-materialize';
+
+import './styles.scss';
 
 const App = () =>
-  <Dropdown trigger={
-    <Button>Drop me!</Button>
-  }>
-    <p>Hello</p>
-    <p>Hello</p>
-    <p>Hello</p>
-    <p>Bye</p>
-  </Dropdown>;
+  <div>
+    <Navbar brand="logo" right>
+      <NavItem href="#"><Icon>search</Icon></NavItem>
+      <NavItem href="#"><Icon>view_module</Icon></NavItem>
+      <NavItem href="#"><Icon>refresh</Icon></NavItem>
+      <NavItem href="#"><Icon>more_vert</Icon></NavItem>
+    </Navbar>
+    <Breadcrumb>
+      <MenuItem>first</MenuItem>
+      <MenuItem>second</MenuItem>
+      <MenuItem>third</MenuItem>
+    </Breadcrumb>
+    <Dropdown trigger={
+      <Button>Drop me!</Button>
+    }>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Hello</p>
+      <p>Bye</p>
+    </Dropdown>
+  </div>;
 
 ReactDOM.render(
   <App />, document.getElementById('application')
